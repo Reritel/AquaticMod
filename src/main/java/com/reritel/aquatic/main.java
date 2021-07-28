996f97320b950e8baa6756e32ffd29955faf6c76
Oleg Oleinikov <oleg@faem.me>
2021-07-28 23:07:07 +0300

    test item

--- src/main/java/com/reritel/aquatic/main.java
+++ src/main/java/com/reritel/aquatic/main.java
@@ -2,6 +2,11 @@ package com.reritel.aquatic;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,9 +18,13 @@ public class main implements ModInitializer {
     public static final String MOD_ID = "aquatic";
     public static final String MOD_NAME = "AquaticMod";
 
+    public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
+
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
         //TODO: Initializer
     }
 
